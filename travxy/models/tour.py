09b5393416d48f15e92fdfa7fc0b4f489bf521ec
Imisioluwa Akande <imisioluwa.akande@andela.com>
2022-08-12 00:54:27 -0400

    refactor tour module

--- travxy/models/tour.py
+++ travxy/models/tour.py
@@ -13,23 +13,27 @@ class TourModel(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id', ondelete="CASCADE"))
     category = db.relationship('CategoryModel', back_populates="tours")
 
-    def __init__(self, name, location, country, about, category_id):
-        self.name = name
-        self.location = location
-        self.country = country
-        self.about = about
-        self.category_id = category_id
+    # def __init__(self, name, location, country, about, category_id):
+    #     self.name = name
+    #     self.location = location
+    #     self.country = country
+    #     self.about = about
+    #     self.category_id = category_id
 
     def json(self):
-        return{'name': self.name, 'location': self.location, 'country': self.country, 'about': self.about, 'category_id': self.category_id}
+        return {'name': self.name, 'location': self.location, 'country': self.country, 'about': self.about, 'category_id': self.category_id}
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
 
     @classmethod
-    def find_by_id(cls, tour_id):
-        return cls.query.filter_by(id=tour_id).first()
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
